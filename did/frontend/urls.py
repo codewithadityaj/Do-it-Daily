8a38334e7936f603413e668d56c82273aed21317
Aditya Ashok Jadhav <adityajadhav11193@gmail.com>
2025-08-16 01:53:56 +0530

    Updated templates and views

--- did/frontend/urls.py
+++ did/frontend/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('signup/', views.signup_page, name='signup'),
-    path('index/', views.index_page, name='index')
+    path('index/', views.index_page, name='index'),
+    path('child/', views.child_page, name='child'),
+    path('teen/', views.teen_page, name='teen'),
+    path('adult/', views.adult_page, name='adult'),
+    path('child/rewards/', views.child_rewards_page, name='child_rewards'),
+    path('child/progress/', views.child_progress_page, name='child_progress'),
+    path('child/challenges/', views.child_challenge_page, name='child_challenge'),
 ]
--- did/frontend/views.py
+++ did/frontend/views.py
@@ -11,3 +11,39 @@ def signup_page(request):
 
 def index_page(request):
     return render(request, 'index.html')
+
+def child_page(request):
+    return render(request, 'child.html')
+
+def teen_page(request):
+    return render(request, 'teen.html')
+
+def adult_page(request):
+    return render(request, 'adult.html')
+
+def features_page(request):
+    return render(request, 'features.html')
+
+def security_page(request):
+    return render(request, 'security.html')
+
+def teen_challenge_page(request):
+    return render(request, 'teen-challenge.html')
+
+def adult_challenge_page(request):
+    return render(request, 'adult-challenge.html')
+
+def child_challenge_page(request):
+    return render(request, 'child-challenge.html')
+
+def teen_progress_page(request):
+    return render(request, 'teen-progress.html')
+
+def adult_progress_page(request):
+    return render(request, 'adult-progress.html')
+
+def child_progress_page(request):
+    return render(request, 'child-progress.html')
+
+def child_rewards_page(request):
+    return render(request, 'child-rewards.html')
